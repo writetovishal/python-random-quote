2b00a357d4593b079bae957eedcfe3d313938067
writetovishal <85872586+writetovishal@users.noreply.github.com>
2021-07-10 23:36:37 +0530

    File writing programatically

--- get-quote.py
+++ get-quote.py
@@ -2,12 +2,18 @@ import random
 def primary():
   #print("Keep it logically awesome.")
 
+  f = open("quotes.txt", "a")
+  f.write("\nSample quote")
+  #quotes = f.readlines()
+  f.close()
   f = open("quotes.txt")
+  #f.write("Sample quote")
   quotes = f.readlines()
   f.close()
   last = len(quotes) - 1
   rnd = random.randint(0, last)
   print(quotes[rnd])
+  print(quotes[last])
 
 if __name__== "__main__":
   primary()
